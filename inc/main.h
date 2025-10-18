f319fa9af40cad78da40147ff9272ab2d0716d8b
FeelYon <tokpli33@mail.ru>
2025-10-18 23:57:35 +0400

    v0.2

--- inc/main.h
+++ inc/main.h
@@ -3,10 +3,10 @@
 #define FIELD_WIDTH 50 // Ширина поля
 #define FIELD_HEIGHT 20 // Высота поля
 
-#define GENERATIONS 10000 // Всего поколений
+#define GENERATIONS 100000 // Всего поколений
 
 #define INIT_POP_SIZE 30 // Размер популяции
-#define SKIP_GENERATIONS 100 // Кол-во пропусков раундов/поколений
+#define SKIP_GENERATIONS 1000 // Кол-во пропусков раундов/поколений
 #define NUMBER_OF_STEPS 50 // Кол-во шагов в раунде
 #define INIT_ENERGY_AGENT 100 // Кол-во энергии у агента
 #define ENERGY_LOSS_PER_STEP 5 // Потеря энергии за шаг //
@@ -18,7 +18,7 @@
 #define CHANCE_OF_FOOD_APPEARANCE 0.33f //0.5f Шанс появления еды в клетке
 #define ENERGY_FOOD_VALUE 40 // Энергетическая ценность еды //
 
-#define TICK_MS 125 // Интервал между тиками (мс)
+#define TICK_MS 100 // Интервал между тиками (мс)
 
 #define USE_A_NEURAL_NETWORK 1 // Отвечает за использование нейросети в агентах
 #define INPUT_VALUES 11 // Входные значения
@@ -26,7 +26,7 @@
 #define NEURONS_IN_HIDDEN_LAYER 13 // Кол-во нейронов в скрытых(ом) слоях(е) // (одинаково)
 #define OUTPUT_VALUES 4 // Выходные значения
 
-#define AGENT_MUTATION_CHANCE 0.2f // Шанс мутации гена
+#define AGENT_MUTATION_CHANCE 0.05f // Шанс мутации гена
 #define AGENT_MUTATION_POWER 0.15f // Число-диапозон (+, -), которое суммируется с каждым весом
 #define AGENT_CHANCE_TO_CROSS_OVER 0.5f // Шанс скрещивания (кроссинговера)
 
